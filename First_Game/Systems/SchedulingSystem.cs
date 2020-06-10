3db3b0323d9101180b4a8718de439de9e22d8c4b
Pierce Connaughton <S00185812@mail.itsligo.ie>
2020-06-10 15:25:34 +0100

    We created a schedule interface and all the methods we are gonna use for our schedules so that we can now have the game be turn based we now have too implement these schedules into the game

---
+++ First_Game/Systems/SchedulingSystem.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using First_Game.Interfaces;
+
+namespace First_Game.Systems
+{
+    public class SchedulingSystem
+    {
+        private int _time;
+        private readonly SortedDictionary<int, List<IScheduleable>> _scheduleables;
+
+        public SchedulingSystem()
+        {
+            //the time starts at 0 and we create a new list of scheduables
+            _time = 0;
+            _scheduleables = new SortedDictionary<int, List<IScheduleable>>();
+        }
+
+        //this adds a new object too the schedule
+        //we place it at the current time plus the objects time property
+        public void Add(IScheduleable scheduleable)
+        {
+            //gets the key time
+            int key = _time + scheduleable.Time;
+
+            //if the list does not contain a schedule at this time create a new schedule
+            if (!_scheduleables.ContainsKey(key))
+            {
+                _scheduleables.Add(key, new List<IScheduleable>());
+            }
+            //add this schedule too the list of schedules at this time
+            _scheduleables[key].Add(scheduleable);
+        }
+
+        //this removes an object from the schedule
+        //used too remove a monsters schedule after it is killed etc
+        public void Remove(IScheduleable scheduleable)
+        {
+            //this represents a key in the dictionary which represents a time
+            //we have a list of them because multiple actors can act at the same time
+            KeyValuePair<int, List<IScheduleable>> scheduleableListFound = new KeyValuePair<int, List<IScheduleable>>(-1, null);
+
+            //for each schedule in the lisr of schedules find this particular schedule
+            foreach (var scheduleableList in _scheduleables)
+            {
+                if (scheduleableList.Value.Contains(scheduleable))
+                {
+                    scheduleableListFound = scheduleableList;
+                    break;
+                }
+            }
+
+            //if the particular schedule is found remove it fom the list of schedules
+            if (scheduleableListFound.Value != null)
+            {
+                scheduleableListFound.Value.Remove(scheduleable);
+                if (scheduleableListFound.Value.Count <= 0)
+                {
+                    _scheduleables.Remove(scheduleableListFound.Key);
+                }
+            }
+        }
+
+        //get the next object wgis turn it is from the schedule. Advance time if necessary
+        public IScheduleable Get()
+        {
+            var firstScheduleGroup = _scheduleables.First();
+            var firstScheduleable = firstScheduleGroup.Value.First();
+
+            Remove(firstScheduleable);
+            _time = firstScheduleGroup.Key;
+            return firstScheduleable;
+        }
+
+        //Get the current time for the schedule
+        public int GetTime()
+        {
+            return _time;
+        }
+
+        //resets the time and clears the schedule
+        public void Clear()
+        {
+            _time = 0;
+            _scheduleables.Clear();
+        }
+    }
+}
